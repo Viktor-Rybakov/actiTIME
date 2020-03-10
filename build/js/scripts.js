3b64d87583dd1aeb976332edb815c24aff0eb40c
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-03-10 12:27:49 +0300

    Сделал адаптивным обработчик событий сладера

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -15,8 +15,28 @@ if ( document.documentElement.clientWidth >= 1200 ) {
   controls[0].classList.add('controls__button--choosed');
   controls[1].classList.add('controls__button--hidden');
   controls[3].classList.add('controls__button--hidden');
+}
 
-  controls[0].onclick = function() {
+controls[0].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('visually-hidden');
+    features[1].classList.add('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+
+  if ( document.documentElement.clientWidth >= 1200 ) {
     controls[0].classList.add('controls__button--choosed');
     controls[1].classList.remove('controls__button--choosed');
     controls[2].classList.remove('controls__button--choosed');
@@ -32,8 +52,48 @@ if ( document.documentElement.clientWidth >= 1200 ) {
     features[2].classList.add('visually-hidden');
     features[3].classList.add('visually-hidden');
   }
+}
 
-  controls[2].onclick = function() {
+controls[1].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.add('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.add('visually-hidden');
+    features[1].classList.remove('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+}
+
+controls[2].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.add('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.add('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.add('visually-hidden');
+    features[1].classList.add('visually-hidden');
+    features[2].classList.remove('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+
+  if ( document.documentElement.clientWidth >= 1200 ) {
     controls[0].classList.remove('controls__button--choosed');
     controls[1].classList.remove('controls__button--choosed');
     controls[2].classList.add('controls__button--choosed');
@@ -51,59 +111,9 @@ if ( document.documentElement.clientWidth >= 1200 ) {
   }
 }
 
-if ( document.documentElement.clientWidth < 1200 ) {
-  controls[0].onclick = function() {
-    controls[0].classList.add('controls__button--choosed');
-    controls[1].classList.remove('controls__button--choosed');
-    controls[2].classList.remove('controls__button--choosed');
-    controls[3].classList.remove('controls__button--choosed');
-  
-    features[0].classList.add('slider__list-item--active');
-    features[1].classList.remove('slider__list-item--active');
-    features[2].classList.remove('slider__list-item--active');
-    features[3].classList.remove('slider__list-item--active');
-  
-    features[0].classList.remove('visually-hidden');
-    features[1].classList.add('visually-hidden');
-    features[2].classList.add('visually-hidden');
-    features[3].classList.add('visually-hidden');
-  }
+controls[3].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
 
-  controls[1].onclick = function() {
-    controls[0].classList.remove('controls__button--choosed');
-    controls[1].classList.add('controls__button--choosed');
-    controls[2].classList.remove('controls__button--choosed');
-    controls[3].classList.remove('controls__button--choosed');
-  
-    features[0].classList.remove('slider__list-item--active');
-    features[1].classList.add('slider__list-item--active');
-    features[2].classList.remove('slider__list-item--active');
-    features[3].classList.remove('slider__list-item--active');
-  
-    features[0].classList.add('visually-hidden');
-    features[1].classList.remove('visually-hidden');
-    features[2].classList.add('visually-hidden');
-    features[3].classList.add('visually-hidden');
-  }
-
-  controls[2].onclick = function() {
-    controls[0].classList.remove('controls__button--choosed');
-    controls[1].classList.remove('controls__button--choosed');
-    controls[2].classList.add('controls__button--choosed');
-    controls[3].classList.remove('controls__button--choosed');
-  
-    features[0].classList.remove('slider__list-item--active');
-    features[1].classList.remove('slider__list-item--active');
-    features[2].classList.add('slider__list-item--active');
-    features[3].classList.remove('slider__list-item--active');
-  
-    features[0].classList.add('visually-hidden');
-    features[1].classList.add('visually-hidden');
-    features[2].classList.remove('visually-hidden');
-    features[3].classList.add('visually-hidden');
-  }
-
-  controls[3].onclick = function() {
     controls[0].classList.remove('controls__button--choosed');
     controls[1].classList.remove('controls__button--choosed');
     controls[2].classList.remove('controls__button--choosed');
@@ -121,6 +131,47 @@ if ( document.documentElement.clientWidth < 1200 ) {
   }
 }
 
+window.addEventListener('resize', function(event) {
+  if ( document.documentElement.clientWidth >= 1200 ) {
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('visually-hidden');
+    features[1].classList.remove('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    controls[1].classList.add('controls__button--hidden');
+    controls[3].classList.add('controls__button--hidden');
+  } 
+    else if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    controls[1].classList.remove('controls__button--hidden');
+    controls[3].classList.remove('controls__button--hidden');
+  
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('visually-hidden');
+    features[1].classList.add('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+});
 
 let headerMenu = document.querySelector('.js-header-menu');
 let headerSubMenuList = document.querySelector('.js-header-menu__submenu-list');
@@ -160,3 +211,17 @@ headerSubMenuButton.onclick = function() {
     headerSubMenuList.setAttribute('aria-expanded', true);
   }
 }
+
+// document.body.onclick = function (e) {
+//   e = e || event;
+//   target = e.target || e.srcElement;
+
+
+
+//   // if ( (headerMenu.getAttribute('aria-expanded') == 'true') && !(headerMenu.contains(target)) && buttonOpen.contains(target) && buttonClose.contains(target)) {
+//   //   console.log(target);
+
+//   //   // headerMenu.classList.remove('header-menu--open');
+//   //   // buttonOpen.classList.remove('header__menu-button--hidden');
+//   //   // buttonClose.classList.add('header__menu-button--hidden');
+//   //   // headerMenu.setAttribute('aria-expanded', false);
--- src/js/features.js
+++ src/js/features.js
@@ -15,8 +15,28 @@ if ( document.documentElement.clientWidth >= 1200 ) {
   controls[0].classList.add('controls__button--choosed');
   controls[1].classList.add('controls__button--hidden');
   controls[3].classList.add('controls__button--hidden');
+}
 
-  controls[0].onclick = function() {
+controls[0].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('visually-hidden');
+    features[1].classList.add('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+
+  if ( document.documentElement.clientWidth >= 1200 ) {
     controls[0].classList.add('controls__button--choosed');
     controls[1].classList.remove('controls__button--choosed');
     controls[2].classList.remove('controls__button--choosed');
@@ -32,8 +52,48 @@ if ( document.documentElement.clientWidth >= 1200 ) {
     features[2].classList.add('visually-hidden');
     features[3].classList.add('visually-hidden');
   }
+}
 
-  controls[2].onclick = function() {
+controls[1].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.add('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.add('visually-hidden');
+    features[1].classList.remove('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+}
+
+controls[2].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.add('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.add('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.add('visually-hidden');
+    features[1].classList.add('visually-hidden');
+    features[2].classList.remove('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+
+  if ( document.documentElement.clientWidth >= 1200 ) {
     controls[0].classList.remove('controls__button--choosed');
     controls[1].classList.remove('controls__button--choosed');
     controls[2].classList.add('controls__button--choosed');
@@ -51,59 +111,9 @@ if ( document.documentElement.clientWidth >= 1200 ) {
   }
 }
 
-if ( document.documentElement.clientWidth < 1200 ) {
-  controls[0].onclick = function() {
-    controls[0].classList.add('controls__button--choosed');
-    controls[1].classList.remove('controls__button--choosed');
-    controls[2].classList.remove('controls__button--choosed');
-    controls[3].classList.remove('controls__button--choosed');
-  
-    features[0].classList.add('slider__list-item--active');
-    features[1].classList.remove('slider__list-item--active');
-    features[2].classList.remove('slider__list-item--active');
-    features[3].classList.remove('slider__list-item--active');
-  
-    features[0].classList.remove('visually-hidden');
-    features[1].classList.add('visually-hidden');
-    features[2].classList.add('visually-hidden');
-    features[3].classList.add('visually-hidden');
-  }
+controls[3].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
 
-  controls[1].onclick = function() {
-    controls[0].classList.remove('controls__button--choosed');
-    controls[1].classList.add('controls__button--choosed');
-    controls[2].classList.remove('controls__button--choosed');
-    controls[3].classList.remove('controls__button--choosed');
-  
-    features[0].classList.remove('slider__list-item--active');
-    features[1].classList.add('slider__list-item--active');
-    features[2].classList.remove('slider__list-item--active');
-    features[3].classList.remove('slider__list-item--active');
-  
-    features[0].classList.add('visually-hidden');
-    features[1].classList.remove('visually-hidden');
-    features[2].classList.add('visually-hidden');
-    features[3].classList.add('visually-hidden');
-  }
-
-  controls[2].onclick = function() {
-    controls[0].classList.remove('controls__button--choosed');
-    controls[1].classList.remove('controls__button--choosed');
-    controls[2].classList.add('controls__button--choosed');
-    controls[3].classList.remove('controls__button--choosed');
-  
-    features[0].classList.remove('slider__list-item--active');
-    features[1].classList.remove('slider__list-item--active');
-    features[2].classList.add('slider__list-item--active');
-    features[3].classList.remove('slider__list-item--active');
-  
-    features[0].classList.add('visually-hidden');
-    features[1].classList.add('visually-hidden');
-    features[2].classList.remove('visually-hidden');
-    features[3].classList.add('visually-hidden');
-  }
-
-  controls[3].onclick = function() {
     controls[0].classList.remove('controls__button--choosed');
     controls[1].classList.remove('controls__button--choosed');
     controls[2].classList.remove('controls__button--choosed');
@@ -121,3 +131,44 @@ if ( document.documentElement.clientWidth < 1200 ) {
   }
 }
 
+window.addEventListener('resize', function(event) {
+  if ( document.documentElement.clientWidth >= 1200 ) {
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('visually-hidden');
+    features[1].classList.remove('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    controls[1].classList.add('controls__button--hidden');
+    controls[3].classList.add('controls__button--hidden');
+  } 
+    else if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    controls[1].classList.remove('controls__button--hidden');
+    controls[3].classList.remove('controls__button--hidden');
+  
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('visually-hidden');
+    features[1].classList.add('visually-hidden');
+    features[2].classList.add('visually-hidden');
+    features[3].classList.add('visually-hidden');
+  }
+});

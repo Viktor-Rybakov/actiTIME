98297ae186b7429d4624af026ccb6b5a1bc06f4e
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-03-10 09:24:28 +0300

    Сделал анимацию меню

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -122,3 +122,27 @@ if ( document.documentElement.clientWidth < 1200 ) {
 }
 
 
+let headerMenu = document.querySelector('.js-header-menu');
+let headerSubMenuList = document.querySelector('.js-header-menu__submenu-list');
+let headerSubMenuButton = document.querySelector('.js-header-menu__submenu');
+let buttonOpen = document.querySelector('.js-menu-button--open');
+let buttonClose = document.querySelector('.js-menu-button--close');
+
+if ( document.documentElement.clientWidth < 1024 ) {
+  buttonOpen.onclick = function() {
+    headerMenu.classList.add('header-menu--open');
+    buttonOpen.classList.add('header__menu-button--hidden');
+    buttonClose.classList.remove('header__menu-button--hidden');
+  }
+
+  buttonClose.onclick = function() {
+    headerMenu.classList.remove('header-menu--open');
+    buttonOpen.classList.remove('header__menu-button--hidden');
+    buttonClose.classList.add('header__menu-button--hidden');
+  }
+}
+
+headerSubMenuButton.onclick = function() {
+  headerSubMenuButton.classList.toggle('header-menu__submenu--open');
+  headerSubMenuList.classList.toggle('header-menu__submenu-list--open');
+}

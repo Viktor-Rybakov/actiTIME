e0b6edd5c858999842c00f56553e3ef68afcf25e
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-03-10 10:07:12 +0300

    Добавил aria-атрибуты

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,22 +3,36 @@ let headerSubMenuList = document.querySelector('.js-header-menu__submenu-list');
 let headerSubMenuButton = document.querySelector('.js-header-menu__submenu');
 let buttonOpen = document.querySelector('.js-menu-button--open');
 let buttonClose = document.querySelector('.js-menu-button--close');
+let windowsWidht = document.documentElement.clientWidth;
 
-if ( document.documentElement.clientWidth < 1024 ) {
+window.addEventListener('resize', function(event) {
+  windowsWidht = document.documentElement.clientWidth;
+});
+
+if ( windowsWidht < 1024 ) {
   buttonOpen.onclick = function() {
     headerMenu.classList.add('header-menu--open');
     buttonOpen.classList.add('header__menu-button--hidden');
     buttonClose.classList.remove('header__menu-button--hidden');
+    headerMenu.setAttribute('aria-expanded', true);
   }
 
   buttonClose.onclick = function() {
     headerMenu.classList.remove('header-menu--open');
     buttonOpen.classList.remove('header__menu-button--hidden');
     buttonClose.classList.add('header__menu-button--hidden');
+    headerMenu.setAttribute('aria-expanded', false);
   }
 }
+
 
 headerSubMenuButton.onclick = function() {
   headerSubMenuButton.classList.toggle('header-menu__submenu--open');
   headerSubMenuList.classList.toggle('header-menu__submenu-list--open');
+
+  if (headerSubMenuList.getAttribute('aria-expanded') == 'true') {
+    headerSubMenuList.setAttribute('aria-expanded', false);
+  } else {
+    headerSubMenuList.setAttribute('aria-expanded', true);
+  }
 }
